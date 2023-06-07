cffa89558f5666c3f0b5c5fadda0c224d76d5d29
Cedric Wacker <79398896+cfitzi@users.noreply.github.com>
2023-06-07 22:54:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,12 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 streamlit.dataframe(my_fruit_list)
 
 
-
 streamlit.title("My parent's new fancy diner")
-  streamlit.header("Breakfast Menu") 
-    streamlit.text("🥣 Omega 3 and Blueberry Oatmeal")   
-    streamlit.text("🥗 Kale, Spinach, and Rocket Smoothie")
-    streamlit.text("🐔 Hard-boiled free-range Egg")
-    streamlit.text("🥑🍞 Avocado Toast")
+streamlit.header("Breakfast Menu") 
+streamlit.text("🥣 Omega 3 and Blueberry Oatmeal")   
+streamlit.text("🥗 Kale, Spinach, and Rocket Smoothie")
+streamlit.text("🐔 Hard-boiled free-range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
