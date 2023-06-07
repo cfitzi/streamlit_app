a923bf581d02f23d2c2b65dc883f4c62fe2bd584
Cedric Wacker <79398896+cfitzi@users.noreply.github.com>
2023-06-07 22:54:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,18 @@
-import streamlit 
-streamlit.title("My parent's new fancy diner")
+import streamlit
+import pandas 
 
-streamlit.header("Breakfast Menu") 
-streamlit.text("🥣 Omega 3 and Blueberry Oatmeal")   
-streamlit.text("🥗 Kale, Spinach, and Rocket Smoothie")
-streamlit.text("🐔 Hard-boiled free-range Egg")
-streamlit.text("🥑🍞 Avocado Toast")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
+
+
+streamlit.title("My parent's new fancy diner")
+  streamlit.header("Breakfast Menu") 
+    streamlit.text("🥣 Omega 3 and Blueberry Oatmeal")   
+    streamlit.text("🥗 Kale, Spinach, and Rocket Smoothie")
+    streamlit.text("🐔 Hard-boiled free-range Egg")
+    streamlit.text("🥑🍞 Avocado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
