13448210766c317c2ae63d8674edfd88dd432d63
Cedric Wacker <79398896+cfitzi@users.noreply.github.com>
2023-06-08 13:01:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,7 @@ streamlit.dataframe(fruits_to_show)
 # Make API call, parse out json
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # Create dataframe for parsed data 
 streamlit.dataframe(fruityvice_normalized)
