f5ae08bba28fe78053c7b5053c6f25aceca9651f
Cedric Wacker <79398896+cfitzi@users.noreply.github.com>
2023-06-08 12:59:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,6 @@ streamlit.dataframe(fruits_to_show)
 
 # Make API call, parse out json and display results
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = request.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
