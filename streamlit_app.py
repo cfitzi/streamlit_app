02434255a37f98f608a3de24d985083c161b7ac8
Cedric Wacker <79398896+cfitzi@users.noreply.github.com>
2023-06-08 13:00:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,10 +26,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #Show only selected 
 streamlit.dataframe(fruits_to_show)
 
-# Make API call, parse out json and display results
+# Make API call, parse out json
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # Create dataframe for parsed data 
 streamlit.dataframe(fruityvice_normalized)
