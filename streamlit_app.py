f2506301088046925d656806a09ac23932eeceed
Cedric Wacker <79398896+cfitzi@users.noreply.github.com>
2023-06-08 19:47:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,14 +30,20 @@ streamlit.dataframe(fruits_to_show)
 
 # Make API call, parse out json
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-# Create and show dataframe for parsed data 
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get more information.")
+    else: 
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # Create and show dataframe for parsed data 
+      streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
+  #streamlit.write('The user entered ', fruit_choice)
 
 #stop command
 streamlit.stop()
